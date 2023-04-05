98c829db92afe8dce4399fa4ad6b485c044ce0e5
Shibu Gope <shibugoper1@gmail.com>
2023-04-05 10:50:04 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // myProducts.filter((item)=>item.title.includes(search.value))
 
 // myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+
+// fetch("https://fakestoreapi.com/products").then((response)=>{
+//     return response.json()
+// }).then((data)=>{
+//     console.log(data[0]);
+// })
